0d4f9945b67d7cc9c2cddf9e69dc444e2c5c00fe
Diggers89 <matt.dignum@googlemail.com>
2020-10-23 18:26:15 +0100

    refactor cleaner code out to seperate file

--- cmd/pod-janitor.go
+++ cmd/pod-janitor.go
@@ -1,16 +1,11 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"log"
 	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	corev1 "k8s.io/api/core/v1"
-
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/rest"
+	cleaner "github.com/filetrust/pod-janitor/pkg"
 )
 
 var podNamespace = flag.String("pod-namespace", "", "The kubernetes namespace to run in")
@@ -19,80 +14,18 @@ var deleteFailedAfter = flag.Duration("delete-failed-after", 0*time.Minute, "The
 
 func main() {
 	flag.Parse()
-	
+
 	if *podNamespace == "" {
 		log.Fatalf("init failed: pod-namespace argument not set")
 	}
 
-	config, err := rest.InClusterConfig()
+	cleanerArgs, err := cleaner.NewCleanerArgs(*podNamespace, *deleteSuccessfulAfter, *deleteFailedAfter)
 	if err != nil {
-		log.Fatalf("Failed to get cluster config: %v", err)
+		log.Fatalf("Failed to initialise Cleaner: %v", err)
 	}
 
-	client, err := kubernetes.NewForConfig(config)
+	err = cleanerArgs.RunCleaner()
 	if err != nil {
-		log.Fatalf("Failed to initialise client: %v", err)
+		log.Fatalf("Failed to run Cleaner: %v", err)
 	}
-
-	succeededPods, err := client.CoreV1().Pods(*podNamespace).List(context.TODO(), metav1.ListOptions{FieldSelector: "status.phase=Succeeded"})
-	if err != nil {
-		log.Fatalf("Failed to get list of successful pods: %v", err)
-	}
-
-	failedPods, err := client.CoreV1().Pods(*podNamespace).List(context.TODO(), metav1.ListOptions{FieldSelector: "status.phase=Failed"})
-	if err != nil {
-		log.Fatalf("Failed to get list of failed pods: %v", err)
-	}
-
-	pods := succeededPods.Items[:0]
-	pods = append(pods, succeededPods.Items[:]...)
-	pods = append(pods, failedPods.Items[:]...)
-
-	for _, pod := range pods {
-		
-		if shouldDeletePod(&pod){
-			err := client.CoreV1().Pods(*podNamespace).Delete(context.TODO(), pod.Name, metav1.DeleteOptions{})
-
-			if err != nil {
-				log.Printf("Failed to delete pod: %s %v", pod.Name, err)
-				continue
-			}
-			
-			log.Printf("Cleaned up pod %s", pod.Name)
-		}
-	}
-}
-
-func shouldDeletePod(pod *corev1.Pod) bool {
-	podFinishTime := podFinishTime(pod)
-
-	if !podFinishTime.IsZero(){
-		age := time.Since(podFinishTime)
-
-		switch pod.Status.Phase {
-		case corev1.PodSucceeded:
-			if (*deleteSuccessfulAfter > 0 && age >= *deleteSuccessfulAfter){
-				return true
-			}
-		case corev1.PodFailed:
-			if (*deleteFailedAfter > 0 && age >= *deleteFailedAfter) {
-				return true
-			}
-		default:
-			return false
-		}
-	}
-
-	return false
-}
-
-func podFinishTime(podObj *corev1.Pod) time.Time {
-	for _, pc := range podObj.Status.Conditions {
-		// Looking for the time when pod's condition "Ready" became "false" (equals end of execution)
-		if pc.Type == corev1.PodReady && pc.Status == corev1.ConditionFalse {
-			return pc.LastTransitionTime.Time
-		}
-	}
-
-	return time.Time{}
 }

8454ab1d966f6186d8a9ddf8b910c048ef548c47
Diggers89 <matt.dignum@googlemail.com>
2020-10-23 18:30:05 +0100

    update to arg descriptions

--- cmd/pod-janitor.go
+++ cmd/pod-janitor.go
@@ -9,8 +9,8 @@ import (
 )
 
 var podNamespace = flag.String("pod-namespace", "", "The kubernetes namespace to run in")
-var deleteSuccessfulAfter = flag.Duration("delete-successful-after", 0*time.Minute, "The kubernetes namespace to run in")
-var deleteFailedAfter = flag.Duration("delete-failed-after", 0*time.Minute, "The kubernetes namespace to run in")
+var deleteSuccessfulAfter = flag.Duration("delete-successful-after", 0*time.Minute, "Delete pods in succeeded state after X duration (golang duration format, e.g 5m), 0 - never delete")
+var deleteFailedAfter = flag.Duration("delete-failed-after", 0*time.Minute, "Delete pods in failed state after X duration (golang duration format, e.g 5m), 0 - never delete")
 
 func main() {
 	flag.Parse()
